798b56dcd9bdf3d12b13d2cee652a2944064a707
Pierce Woods <42998429+InspectorPocket@users.noreply.github.com>
2019-04-16 16:29:10 +0100

    Updated gulpfile.js with done
    thanks Chris ;)

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('styles', function() {
+gulp.task('styles', function(done) {
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css/'))
+        done()
 });
 
 //Watch task
