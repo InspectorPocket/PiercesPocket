7acbc7b9d099b2df684551153500ecf12bec8eff
Pierce <pierce.woods95@gmail.com>
2020-03-22 18:39:50 +0000

    [development] Nav Bar 1.0
    Nav bar now has fixed spacing between buttons.
    Open and close animations done/
     - Icon animation could use some work.

--- js/nav.js
+++ js/nav.js
@@ -1,21 +1,42 @@
-const header = document.querySelector('.header');
-const iconClosed = document.querySelector('.nav__behind__icon--closed');
-const iconOpen = document.querySelector('.nav__behind__icon--open');
+const nav = document.getElementsByClassName('nav');
+const nav_btn = document.getElementById('nav_btn');
+const navToggle = 'nav__behind__icon--toggled';
+let navBars = [];
+const filterHeader = () => {
+    Array.from(nav)
+        .forEach(item => {
+            navBars.push(item);
+        }
+    );
+}
 
-iconClosed.addEventListener('click', e => {
-    e.preventDefault();
-    
-    // console.log('Closing...');
-    header.classList.add('header--closed');
-    iconClosed.classList.add('hide');
-    iconOpen.classList.remove('hide');
-});
+filterHeader();
+const nav_front = navBars[0];
+const nav_back = navBars[1];
+let nav_width = navBars[0].offsetWidth;
 
-iconOpen.addEventListener('click', e => {
-    e.preventDefault();
-    
-    // console.log('Opening...');
-    header.classList.remove('header--closed');
-    iconClosed.classList.remove('hide');
-    iconOpen.classList.add('hide');
+nav_btn.addEventListener('click', () => {
+    const att_sm = `transform: translateX(calc(-${nav_width}px + 3.75em));`;
+    const att_lg = `transform: translateX(calc(-${nav_width}px + 5.2em));`;
+    if (nav_btn.classList.contains(navToggle)) {
+        navBars.forEach(nav => {
+            if (window.innerWidth < 768) {
+                nav.removeAttribute('style', att_sm);
+            }
+            else if (window.innerWidth > 768) {
+                nav.removeAttribute('style', att_lg);
+            }
+        });
+        nav_btn.classList.remove(navToggle);
+    } else {
+        navBars.forEach(nav => {
+            if (window.innerWidth < 768) {
+                nav.setAttribute('style', att_sm);
+            }
+            else if (window.innerWidth > 768) {
+                nav.setAttribute('style', att_lg);
+            }
+        });
+        nav_btn.classList.add(navToggle);
+    }
 });
