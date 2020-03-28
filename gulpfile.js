d5c423b29de86808e415c58fa1e514f2bb5e8210
Pierce <pierce.woods95@gmail.com>
2020-03-28 18:39:49 +0000

    [development] Moved into app folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 var gulp = require('gulp');
+var nunjucksRender = require('gulp-nunjucks-render');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function(done) {
-    gulp.src('sass/**/*.scss')
+    gulp.src('app/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'))
+        .pipe(gulp.dest('./app/'))
         done()
 });
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',gulp.series('styles'));
+    gulp.watch('app/sass/**/*.scss',gulp.series('styles'));
 });
