c2cda33b668ed6e26f5fe77eb0170293710b9249
Pierce <pierce.woods95@gmail.com>
2020-03-29 17:23:53 +0100

    [development] App Folder 0.1
    Index.html is now on the outside of app.

--- gulpfile.js
+++ gulpfile.js
@@ -9,20 +9,34 @@ gulp.task('styles', function(done) {
         done()
 });
 
+// Nunjucks task
+gulp.task('nunjucks', function() {
+    // Gets .html and .nunjucks files in pages
+    return gulp.src('app/pages/**/*.+(html|njk)', '!app/pages/index.njk')
+    // Renders template with nunjucks
+    .pipe(nunjucksRender({
+        path: ['app/templates']
+      }))
+    // output files in app folder
+    .pipe(gulp.dest('app'))
+  });
+  
+  // Nunjucks Index
+  gulp.task('njk-index', function() {
+    // Gets index.njk
+    return gulp.src('app/pages/index.+(html|njk)')
+    // Renders template with nunjucks
+    .pipe(nunjucksRender({
+        path: ['app/templates']
+      }))
+    // output files in app folder
+    .pipe(gulp.dest('./'))
+  });
+
+  //TODO make index not appear in app || make site load with all files in app
 // Watch task
 gulp.task('default',function() {
     gulp.watch('app/sass/**/*.scss',gulp.series('styles'));
+    gulp.watch('app/pages/index.njk',gulp.series('njk-index'));
     gulp.watch('app/**/**/*.njk',gulp.series('nunjucks'));
-});
-
-// Nunjucks task
-gulp.task('nunjucks', function() {
-  // Gets .html and .nunjucks files in pages
-  return gulp.src('app/pages/**/*.+(html|njk)')
-  // Renders template with nunjucks
-  .pipe(nunjucksRender({
-      path: ['app/templates']
-    }))
-  // output files in app folder
-  .pipe(gulp.dest('app'))
 });
